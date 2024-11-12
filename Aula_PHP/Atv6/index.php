d2e50d1df9c7067af5a599d1d1994292c3b47096
SarahVeiga <sarahveiga55@gmail.com>
2024-11-12 15:30:46 -0300

    Atividades da Aula PHP

---
+++ Aula_PHP/Atv6/index.php
@@ -0,0 +1,25 @@
+<?php
+
+$Dia =1
+switch ($Dia) {
+    case '1':
+     echo "Domingo";
+        break;
+
+    case '2':
+     echo "Segunda";
+        break;
+
+    case '3':
+    echo "Terça";
+              break;
+
+    case '4':
+         echo "Quarta";
+             break;
+
+        
+
+     default:
+        echo "Escolha um numero de 1 a 7"
+}
